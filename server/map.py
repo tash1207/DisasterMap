dc36cf258528f5ae8b5d951693b2c82ef143d086
Sargun Kaur <sargunjot.kaur@gmail.com>
2016-05-14 19:03:23 -0700

    sorting maps

--- server/map.py
+++ server/map.py
@@ -18,14 +18,16 @@ class Map():
 		disasters = self.db.disasters
 		drange = current_date - timedelta(days=10)
 		# Get nearest disaster map from the last ten days within 10,000 meters.
-		result_map = disasters.find_one(
-			{"start_datetime": {"$gte": drange},
-			 "loc": SON([("$near", location), ("$maxDistance", 1)])})
+		result_map = disasters.find_one({"start_datetime": {"$gte": drange}, "loc": SON([("$near", location), ("$maxDistance", 10)])})
 		if result_map:
+			print "START"
 			print result_map["_id"]
 			print result_map
+			print "END"
 			return result_map["_id"]
+			
 		else:
+			print "OH NOSE"
 			return self.create_map(current_date, location)
 
 	def create_map(self, current_date, location):
@@ -33,11 +35,11 @@ class Map():
 		# Insert the new disaster map and other data in the disaster db
 		disasters.create_index([("loc", pymongo.GEO2D), ("name", pymongo.ASCENDING), ("start_datetime", pymongo.DESCENDING)])
 		try:
-			response = disasters.insert_one({"loc" : location, "name": "blue", "start_datetime": datetime.now()})
+			response = disasters.insert_one({'loc': location, "name": "red", "start_datetime": datetime.now()})
+			#response = disasters.insert_one({'loc': {'type': 'Point', 'coordinates': location}, "name": "blue", "start_datetime": datetime.now()})
 			print response.inserted_id
 			print response
 			return response.inserted_id
-			# return response.inserted_id	
 		except:
 			print "Unexpected error:", sys.exc_info()	
 		return	
--- server/map_test.py
+++ server/map_test.py
@@ -7,7 +7,7 @@ class TestMap(unittest.TestCase):
 	def test_existing_map(self):
 		db = MongoClient().geo_example
 		db.disasters.create_index([("loc", GEO2D)])
-		result = db.disasters.insert_many([{"loc": [12, 15], "name": "blah", "start_datetime": datetime.now()}, {"loc": [30, 5], "name": "blah2", "start_datetime": datetime.now()}, {"loc": [1, 2], "name": "blah3", "start_datetime": datetime.now()}, {"loc": [4, 4], "name": "blah4", "start_datetime": datetime.now()}]) 
+		result = db.disasters.insert_many([{"loc": [12, 15], "name": "blah1.44", "start_datetime": datetime.now()}, {"loc": [30, 5], "name": "blah2.222", "start_datetime": datetime.now()}, {"loc": [1, 2], "name": "blah3111", "start_datetime": datetime.now()}, {"loc": [40, 40], "name": "blah4333", "start_datetime": datetime.now()}]) 
 		print result.inserted_ids
 		m = Map(db)
 		m.get_map_id(datetime.now(), [0, 0])
